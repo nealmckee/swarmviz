c5d972cb24e764cfb38867cfcfc26cf7bd146616
Neal McKee <mail@nealmckee.com>
2023-12-01 02:37:19 +0100

    move metrics to their own file

---
+++ src/metrics.jl
@@ -0,0 +1,32 @@
+function swarm_polarisation(tracking_datapoint; cluster_members=[])
+    if !isempty(cluster_members)
+        tracking_datapoint = tracking_datapoint[cluster_members, :]
+    end
+    return norm(sum(tracking_datapoint[:, 6:7]; dims=1)) / size(tracking_datapoint, 1)
+end
+
+# Calculate the rotational order of the swarm for the data of one timestep
+function swarm_rotational_order(tracking_datapoint; cluster_members=[])
+    if !isempty(cluster_members)
+        tracking_datapoint = tracking_datapoint[cluster_members, :]
+    end
+    clustersize = size(tracking_datapoint, 1)
+    barycenter = sum(tracking_datapoint[:, [2, 4]]; dims=1) ./ clustersize
+    radial_unit_vectors = mapslices(
+        x -> x / norm(x), tracking_datapoint[:, [2, 4]] .- barycenter; dims=2
+    )
+    return norm(
+        sum([
+            cross(
+                push!(tracking_datapoint[i, 6:7], 0), push!(radial_unit_vectors[i, :], 0)
+            ) for i in 1:clustersize
+        ]),
+    ) / clustersize
+end
+
+function swarm_mean_interindividual_distance(tracking_datapoint, cluster_members=[])
+    if !isempty(cluster_members)
+        tracking_datapoint = tracking_datapoint[cluster_members, :]
+    end
+    return mean(pairwise(Euclidean(), tracking_datapoint[:, [2, 4]]; dims=1))
+end
--- swarmviz.jl
+++ swarmviz.jl
@@ -6,6 +6,8 @@ using LinearAlgebra
 using NPZ
 using Statistics
 
+include("src/metrics.jl")
+
 experiment_file = "data/DatasetE2/E21/E212/E212r1_summaryd.npy"
 wall_file = "data/DatasetE2/ArenaBorders/ArenaBorders_r1_summaryd.npy"
 
@@ -29,40 +31,6 @@ tracking_data = cat(
     tracking_data, reshape(sin.(tracking_data[:, 5, :]), n_robots, 1, n_timesteps); dims=2
 )
 
-# Calculate the polarisation of the swarm for the data of one timestep
-function swarm_polarisation(tracking_datapoint; cluster_members=[])
-    if !isempty(cluster_members)
-        tracking_datapoint = tracking_datapoint[cluster_members, :]
-    end
-    return norm(sum(tracking_datapoint[:, 6:7]; dims=1)) / size(tracking_datapoint, 1)
-end
-
-# Calculate the rotational order of the swarm for the data of one timestep
-function swarm_rotational_order(tracking_datapoint; cluster_members=[])
-    if !isempty(cluster_members)
-        tracking_datapoint = tracking_datapoint[cluster_members, :]
-    end
-    clustersize = size(tracking_datapoint, 1)
-    barycenter = sum(tracking_datapoint[:, [2, 4]]; dims=1) ./ clustersize
-    radial_unit_vectors = mapslices(
-        x -> x / norm(x), tracking_datapoint[:, [2, 4]] .- barycenter; dims=2
-    )
-    return norm(
-        sum([
-            cross(
-                push!(tracking_datapoint[i, 6:7], 0), push!(radial_unit_vectors[i, :], 0)
-            ) for i in 1:clustersize
-        ]),
-    ) / clustersize
-end
-
-function swarm_mean_interindividual_distance(tracking_datapoint, cluster_members=[])
-    if !isempty(cluster_members)
-        tracking_datapoint = tracking_datapoint[cluster_members, :]
-    end
-    return mean(pairwise(Euclidean(), tracking_datapoint[:, [2, 4]]; dims=1))
-end
-
 # precalculate all metrics for all timesteps (currently no clustering)
 polarisation = dropdims(
     mapslices(swarm_polarisation, tracking_data; dims=(1, 2)); dims=(1, 2)
@@ -73,7 +41,6 @@ rotational_order = dropdims(
 mean_interindividual_distance = dropdims(
     mapslices(swarm_mean_interindividual_distance, tracking_data; dims=(1, 2)); dims=(1, 2)
 )
-
 metrics_data = cat(polarisation, rotational_order, mean_interindividual_distance; dims=2)
 
 # Plot styling
